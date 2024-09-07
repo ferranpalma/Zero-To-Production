7c0af131ea33939360b37e6e963ce139241cbe67
ferranpalma <ferranpalmac@gmail.com>
2024-09-07 12:57:14 +0200

    feat: (migration process) all subscribers have confimed status

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -46,8 +46,8 @@ async fn insert_subscriber_into_database(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
+        INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, 'confirmed')
         "#,
         Uuid::new_v4(),
         subscriber_data.email.as_ref(),
